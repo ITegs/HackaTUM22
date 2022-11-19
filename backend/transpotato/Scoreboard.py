b660eeb2d7dd82c1da036320f4bedbd1cc7b0d6e
janle <janlevinhe@gmail.com>
2022-11-19 20:16:50 +0100

    Scoreboard and added return to mindistance

--- backend/transpotato/Scoreboard.py
+++ backend/transpotato/Scoreboard.py
@@ -3,15 +3,15 @@ import sqlite3 as sqshit
 
 
 def adduser(use, name):
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("INSERT INTO User VALUES(?, ?, ?, ?)", (use, name, 0,0))
+    cur.execute("INSERT INTO snippets_genuser VALUES(?, ?, ?, ?)", (use, name, 0,0))
     con.commit()
 
 def createuser(id, name = "Untitled User"):
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    res = cur.execute("SELECT Id FROM User")
+    res = cur.execute("SELECT id FROM snippets_genuser")
     data = res.fetchall()
     there = False
     for i in data:
@@ -23,55 +23,55 @@ def createuser(id, name = "Untitled User"):
         adduser(id, name)
 
 def showtable():
-    connection = sqshit.connect('data.db')
-    cursor = connection.execute('select * from User')
+    connection = sqshit.connect('db.sqlite3')
+    cursor = connection.execute('select * from snippets_genuser')
     names = list(map(lambda x: x[0], cursor.description))
     print(names)
 
 
 def createtable():
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("CREATE TABLE User(Id, Username, score, TotalDistance)")
+    cur.execute("CREATE TABLE snippets_genuser(Id, Username, score, TotalDistance)")
 
 def addvalue(id, score, distanceadder):
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("SELECT TotalDistance FROM User WHERE Id = ?", (id, ))
+    cur.execute("SELECT TotalDistance FROM snippets_genuser WHERE id = ?", (id, ))
     sofar = cur.fetchall()
     newdistance = sofar[0][0] + distanceadder
-    cur.execute("SELECT score FROM User WHERE Id = ?", (id, ))
+    cur.execute("SELECT score FROM snippets_genuser WHERE id = ?", (id, ))
     sofar = cur.fetchall()
     newscore = sofar[0][0] + score
-    cur.execute("UPDATE User SET TotalDistance = ? WHERE Id = ?;", (newdistance, id))
-    cur.execute("UPDATE User SET score = ? WHERE Id = ?;", (newscore, id))
+    cur.execute("UPDATE snippets_genuser SET TotalDistance = ? WHERE id = ?;", (newdistance, id))
+    cur.execute("UPDATE snippets_genuser SET score = ? WHERE id = ?;", (newscore, id))
     con.commit()
 
 def getscore(id):
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("SELECT score FROM User WHERE Id = ?", (id,))
+    cur.execute("SELECT score FROM snippets_genuser WHERE id = ?", (id,))
     sofar = cur.fetchall()[0][0]
     return sofar
 
 def getdistance(id):
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("SELECT TotalDistance FROM User WHERE Id = ?", (id,))
+    cur.execute("SELECT TotalDistance FROM snippets_genuser WHERE id = ?", (id,))
     sofar = cur.fetchall()[0][0]
     return sofar
 
 def gettop10():
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("SELECT Username, score, TotalDistance FROM User ORDER BY score DESC LIMIT 10 ")
+    cur.execute("SELECT Username, score, TotalDistance FROM snippets_genuser ORDER BY score DESC LIMIT 10 ")
     top = cur.fetchall()
     return(top)
 
 def getmypos(id):
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("SELECT Id FROM User ORDER BY score DESC")
+    cur.execute("SELECT id FROM snippets_genuser ORDER BY score DESC")
     top = cur.fetchall()
     counter = 0
     for i in top:
@@ -80,23 +80,24 @@ def getmypos(id):
             return counter
 
 def changename(id, newname):
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("UPDATE User SET Username = ? WHERE Id = ?;", (newname, id))
+    cur.execute("UPDATE snippets_genuser SET Username = ? WHERE id = ?;", (newname, id))
     con.commit()
 
 def getnewID():
-    con = sqshit.connect("data.db")
+    con = sqshit.connect("db.sqlite3")
     cur = con.cursor()
-    cur.execute("SELECT Id FROM User ORDER BY Id DESC")
+    cur.execute("SELECT id FROM snippets_genuser ORDER BY id DESC")
     new = cur.fetchall()
-    return new[0][0]+1
+    return new[0][0]
 
 #createtable()
 #showtable()
-con = sqshit.connect("data.db")
+con = sqshit.connect("db.sqlite3")
 cur = con.cursor()
 x = cur.fetchall()
+print(getnewID())
 #addvalue(1, 1, 2)
 
 
